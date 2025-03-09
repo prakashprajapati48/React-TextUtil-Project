47760a97a3bc02c3cdf985eddcaa1a2c4e84b073
prajapati prakash <prakashprajapati48480@gmail.com>
2025-03-09 10:58:37 +0530

    Updates

---
+++ precache-manifest.ced27559182228ab2693015d180a7dbb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "318b02cacb084eb387c0850a40236888",
+    "url": "/textutiles/index.html"
+  },
+  {
+    "revision": "24d285f8a515954a7ae6",
+    "url": "/textutiles/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "d27353d7c87c5fc9e645",
+    "url": "/textutiles/static/js/2.fdcb1299.chunk.js"
+  },
+  {
+    "revision": "fc0ba07c0f771d66c44444381376eaf8",
+    "url": "/textutiles/static/js/2.fdcb1299.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "512b39842e926bf575c6",
+    "url": "/textutiles/static/js/3.aa4805c9.chunk.js"
+  },
+  {
+    "revision": "24d285f8a515954a7ae6",
+    "url": "/textutiles/static/js/main.284f1872.chunk.js"
+  },
+  {
+    "revision": "d51d90fc0eef4fafed22",
+    "url": "/textutiles/static/js/runtime-main.7e07d7cb.js"
+  }
+]);
